e46ac1d99a73a8bc9f5bc7da9f1d55f928c5b238
Raphael Vazquez <rvazquez@ids.com.mx>
2020-08-24 04:41:41 -0500

    express send and listen

--- app.js
+++ app.js
@@ -1,7 +1,14 @@
-const http = require("http");
 const express = require("express");
 const app = express();
 
-const server = http.createServer(app);
+app.use((req, res, next) => {
+  console.log("In the middleware ");
+  next();
+});
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log("In another the middleware ");
+  res.send("<h1>Hello from express</h1>");
+});
+
+app.listen(3000);
