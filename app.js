83963e2465b518fbe7c63a66f5df01994b7e7f82
Raphael Vazquez <rvazquez@ids.com.mx>
2020-08-24 04:49:39 -0500

    middlewares on expressjs

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("In the middleware ");
+app.use("/", (req, res, next) => {
+  console.log("this always runs");
   next();
 });
 
-app.use((req, res, next) => {
-  console.log("In another the middleware ");
-  res.send("<h1>Hello from express</h1>");
+app.use("/add-product", (req, res, next) => {
+  console.log("In the middleware ");
+  res.send("<h1>The add product page</h1>");
+});
+
+app.use("/", (req, res, next) => {
+  console.log("In the middleware ");
+  res.send("<h1>Hello from expressjs!</h1>");
 });
 
 app.listen(3000);
