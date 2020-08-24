e3be27bb4d6cfd8c57fec4242494e5d60ff12b36
Raphael Vazquez <rvazquez@ids.com.mx>
2020-08-24 05:01:50 -0500

    bodyparser and get post middlewares

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 const express = require("express");
+const bodyParser = require('body-parser')
 const app = express();
 
-app.use("/", (req, res, next) => {
-  console.log("this always runs");
-  next();
-});
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/add-product", (req, res, next) => {
-  console.log("In the middleware ");
-  res.send("<h1>The add product page</h1>");
+    res.send(
+        "<form action='/product' method='POST'><input type='text' name='title'><button type='submit'>Add</button></form>"
+    );
+});
+
+app.post("/product", (req, res, next) => {
+    console.log(req.body);
+    res.redirect("/");
 });
 
 app.use("/", (req, res, next) => {
-  console.log("In the middleware ");
-  res.send("<h1>Hello from expressjs!</h1>");
+    res.send("<h1>Hello from expressjs!</h1>");
 });
 
 app.listen(3000);
