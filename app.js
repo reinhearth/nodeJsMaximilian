18994182996051b9e0be70b90d7d936b383e6232
Raphael Vazquez <rvazquez@ids.com.mx>
2020-08-24 05:33:13 -0500

    404 html, sendfiles

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
@@ -8,11 +10,11 @@ const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use('/admin',adminRoutes);
+app.use("/admin", adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send("<h1>Page Not Found!</h1>");
+  res.status(404).sendFile(path.join(__dirname, "views", "404.html"));
 });
 
 app.listen(3000);
